f42430f89a2329fcbcd802d15b1d557bc8d84b54
HermannKateu <sontiahermann3@gmail.com>
2023-03-19 14:17:37 +0100

    Adding cors

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,15 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const corsOptions = {
+  "origin": "https://holiday-web-2.netlify.app",
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: corsOptions });
   app.useGlobalPipes(new ValidationPipe(
     {
       whitelist: true,
